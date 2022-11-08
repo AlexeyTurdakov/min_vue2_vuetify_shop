c63ed3bfdb10db98100449699dbe6292bfae6dd6
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-11-08 18:59:01 +0400

    new version + routing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,70 @@
 <template>
-  <h1>HVUE</h1>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary dark>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+            v-for="(link, index) of links"
+            :key="index"
+            :to="link.url"
+          >
+            <v-icon>mdi-bug</v-icon>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      dark
+      prominent
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon @click="toggleDraver"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon v-for="(link, index) in links" :key="index" :to="link.url" class="btn-menu hidden-sm-and-down">
+        {{ link.title }}
+      </v-btn>
+    </v-toolbar>
+  </v-app>
 </template>
 
 <script>
-
 export default {
-  
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "login", icon: "mdi-bug", url: "/login" },
+        { title: "reg", icon: "mdi-bug", url: "/registration" },
+        { title: "order", icon: "mdi-bug", url: "/order" },
+        { title: "new ad", icon: "mdi-bug", url: "/new" },
+        { title: "my ads", icon: "mdi-bug", url: "/list" },
+      ],
+    };
+  },
+
+  methods: {
+    toggleDraver() {
+      this.drawer = !this.drawer;
+    },
+  },
 };
 </script>
+
+<style lang="css" scoped>
+v-toolbar {
+  flex: none;
+}
+
+.btn-menu {
+  margin-right: 40px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import AppHome from "@/components/AppHome.vue";
+import Ad from "@/components/Ads/Ad.vue";
+import AdList from "@/components/Ads/AdList.vue";
+import newAd from "@/components/Ads/NewAd.vue";
+import Login from "@/components/Auth/Login.vue";
+import Registration from "@/components/Auth/Registration.vue";
+import Orders from "@/components/User/Orders.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-]
+  { path: "", name: "home", component: AppHome },
+  { path: "/ad/:id", name: "ad", component: Ad },
+  { path: "/list", name: "adlist", component: AdList },
+  { path: "/new", name: "newad", component: newAd },
+  { path: "/login", name: "login", component: Login },
+  { path: "/registration", name: "registration", component: Registration },
+  { path: "/orders", name: "orders", component: Orders },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+  mode: "history",
+});
 
-export default router
+export default router;
